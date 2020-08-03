6f1429b718384e1db034401337cebce4454dd69a
yoshitaka0729175 <67721834+yoshitaka0729175@users.noreply.github.com>
2020-08-03 17:06:08 +0900

    rspec編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe User do
   describe '#index' do
+
     before do
-      @user = User.save
+      @user = build(:user,nickname: "",email: "", password: "",password_confirmation: "",first_name: "",last_name: "",first_kana: "",last_kana: "",date: "")
     end
  
     it "is valid with a nickname, email, password, password_confirmation, first_kana, last_kana, first_name, last_name, date" do
@@ -15,8 +16,7 @@ describe User do
       #新規登録の時にnicknameが空だと登録不可を試す
       user = build(:user, nickname: nil)
       user.valid? #validatesに引っかかるかどうか
-      binding.pry
-      expect(user.errors[:nickname]).to include("を")
+      expect(user.errors[:nickname]).to include("を入力してください")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     
@@ -24,7 +24,7 @@ describe User do
       #新規登録の時にemailが空だと登録不可を試す
       user = build(:user, email: nil)
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("を入力してください", "を入力してください")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     
@@ -32,7 +32,7 @@ describe User do
       #新規登録の時にnpasswordが空だと登録不可を試す
       user = build(:user, password: nil)
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("を入力してください", "を入力してください", "は不正な値です")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     
@@ -40,21 +40,21 @@ describe User do
       #新規登録の時にnpasswordが空だと登録不可を試す
       user = build(:user, password_confirmation: "")
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません", "を入力してください", "は不正な値です")
       #パスワードと一致しているか確認 doesn't match Password
     end
     it "is invalid without a first_name" do
       #新規登録の時にfirst_nameが空だと登録不可を試す
       user = build(:user, first_name: "")
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:first_name]).to include("can't be blank")
+      expect(user.errors[:first_name]).to include("を入力してください", "は不正な値です")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     it "is invalid without a last_name" do
       #新規登録の時にlast_nameが空だと登録不可を試す
       user = build(:user, last_name: nil)
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:last_name]).to include("can't be blank")
+      expect(user.errors[:last_name]).to include("を入力してください", "は不正な値です")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     
@@ -62,21 +62,21 @@ describe User do
       #新規登録の時にfirst_kanaが空だと登録不可を試す
       user = build(:user, first_kana: nil)
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:first_kana]).to include("can't be blank")
+      expect(user.errors[:first_kana]).to include("を入力してください", "は不正な値です")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     it "is invalid without a last_kana" do
       #新規登録の時にnlast_kanaが空だと登録不可を試す
       user = build(:user, last_kana: nil)
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:last_kana]).to include("can't be blank")
+      expect(user.errors[:last_kana]).to include("を入力してください", "は不正な値です")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     it "is invalid without a date" do
       #新規登録の時にdateが空だと登録不可を試す
       user = build(:user, date: nil)
       user.valid? #validatesに引っかかるかどうか
-      expect(user.errors[:date]).to include("can't be blank")
+      expect(user.errors[:date]).to include("を入力してください")
       #なぜ保存できないのかを返すerrorsメソッド
     end
     
@@ -93,14 +93,14 @@ describe User do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      expect(another_user.errors[:email]).to include("はすでに存在します", "はすでに存在します")
     end
     
     # passwordが5文字以下であれば登録できないこと
     it "is invalid with a password that has less than 5 characters " do
       user = build(:user, password: "00000", password_confirmation: "00000")
       user.valid?
-      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
     end
     
     
