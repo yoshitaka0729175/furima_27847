e02d08e410e0f9f09918e74dddef61864ff7a4bb
yoshitaka0729175 <yoshitaka0729175@gmail.com>
2020-08-07 15:03:14 +0900

    商品出品機能

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,8 +3,6 @@ $(function(){
     let input = $(".price-input").val();
     let tax = Math.round(input*0.1);
     $('#add-tax-price').html(tax);
-    // let profit = input - tax;
-    // $('#profit').html(profit);
     $('#profit').html(input - tax);
   });
 });
