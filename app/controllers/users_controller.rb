f39a228e666dc50752dce84a141b6f6cccdeedea
yoshitaka0729175 <yoshitaka0729175@gmail.com>
2020-08-10 21:44:07 +0900

    heroku修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    #キーワードがなければnilを返す
-    return nil  if params[:keyword] == ""
-    #その文字を含むユーザーを探してきて、@userに代入、ただしログインしている自分は除く。10人まで。
-    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-    respond_to do |format|
+    # #キーワードがなければnilを返す
+    # return nil  if params[:keyword] == ""
+    # #その文字を含むユーザーを探してきて、@userに代入、ただしログインしている自分は除く。10人まで。
+    # @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    # respond_to do |format|
   end
 
   def edit
